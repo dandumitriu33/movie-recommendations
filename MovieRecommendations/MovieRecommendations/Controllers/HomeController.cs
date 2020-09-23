40c2a1afbd9f1707880e86f6298bedbca8be171e
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-23 20:10:36 +0300

    Add Feature - Add movie to database

--- MovieRecommendations/MovieRecommendations/Controllers/HomeController.cs
+++ MovieRecommendations/MovieRecommendations/Controllers/HomeController.cs
@@ -6,16 +6,20 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MovieRecommendations.Models;
+using MoviesDataAccessLibrary.Models;
 
 namespace MovieRecommendations.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRepository _repository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                              IRepository repository)
         {
             _logger = logger;
+            _repository = repository;
         }
 
         public IActionResult Index()
@@ -32,6 +36,23 @@ namespace MovieRecommendations.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovie(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.Add(movie);
+                return RedirectToAction("AddMovie", "Home");
+            }
+            return View(movie);
         }
     }
 }
--- MovieRecommendations/MoviesDataAccessLibrary/Models/Movie.cs
+++ MovieRecommendations/MoviesDataAccessLibrary/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace MoviesDataAccessLibrary.Models
@@ -7,8 +8,14 @@ namespace MoviesDataAccessLibrary.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "Title")]
         public string Title { get; set; }
+        [Required]
+        [Display(Name = "Length in minutes")]
         public int LengthInMinutes { get; set; }
+        [Required]
+        [Display(Name = "Release year")]
         public int ReleaseYear { get; set; }
     }
 }
