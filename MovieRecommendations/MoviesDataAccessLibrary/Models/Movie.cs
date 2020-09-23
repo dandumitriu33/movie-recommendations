7780a3adee4b36e1453320f0d290d4e0ac1d1dd4
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-23 18:48:21 +0300

    Add initial files

---
+++ MovieRecommendations/MoviesDataAccessLibrary/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesDataAccessLibrary.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int LengthInMinutes { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
