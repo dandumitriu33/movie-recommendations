208d02a2587d4284023971ea4c43c40d176fca2a
Dan Dumitriu <dandumitriu33@gmail.com>
2020-09-24 12:27:32 +0300

    Add pictures to movies based on MainGenre

---
+++ MovieRecommendations/MovieRecommendations/Models/Genres.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRecommendations.Models
+{
+    public enum Genres
+    {
+        Adventure,
+        Comedy,
+        Crime,
+        Drama,
+        Fantasy,
+        Horror,
+        Mystery,
+        Romance,
+        SciFi,
+        Western
+    }
+}
