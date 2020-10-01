1281f7b7ba8c2ae600a484f32c84b76653ac52f8
Dan Dumitriu <dandumitriu33@gmail.com>
2020-10-01 11:08:17 +0300

    Add gRPC vs API test logic

--- MovieRecommendations/MovieRecommendationsConsole/Program.cs
+++ MovieRecommendations/MovieRecommendationsConsole/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Grpc.Net.Client;
 using MovieRecommendationsGRPC;
 using System;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace MovieRecommendationsConsole
@@ -9,6 +11,7 @@ namespace MovieRecommendationsConsole
     {
         static async Task Main(string[] args)
         {
+            #region gRPC
             //var input = new HelloRequest { Name = "John" };
 
             //var channel = GrpcChannel.ForAddress("https://localhost:5001");
@@ -20,14 +23,58 @@ namespace MovieRecommendationsConsole
 
             //Console.WriteLine(reply.Message);
 
-            var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var top20MoviesClient = new Top20Movies.Top20MoviesClient(channel);
+            Console.WriteLine("Press any key to start the test.");
+            Console.ReadKey();
 
-            var top20Variable = new Top20MoviesGRPCLookupModel { };
+            long[] gRPCResults = new long[10];
+            for (int i = 0; i < 10; i++)
+            {
+                var stopwatchGRPC = new System.Diagnostics.Stopwatch();
+                stopwatchGRPC.Start();
 
-            var reply = await top20MoviesClient.GetTop20MoviesAsync(top20Variable);
+                var channel = GrpcChannel.ForAddress("https://localhost:5001");
+                var top20MoviesClient = new Top20Movies.Top20MoviesClient(channel);
 
-            Console.WriteLine($"{reply.Json}");
+                var top20Variable = new Top20MoviesGRPCLookupModel { };
+
+                var reply = await top20MoviesClient.GetTop20MoviesAsync(top20Variable);
+
+                stopwatchGRPC.Stop();
+                gRPCResults[i] = stopwatchGRPC.ElapsedMilliseconds;
+                Console.WriteLine($"gRPC fetch time of top 20 movies: {stopwatchGRPC.ElapsedMilliseconds} ms");
+            }
+            Console.WriteLine($"Average gRPC fetch time: {gRPCResults.Average()}");
+
+            // Uncomment to see the complete string with the top 20 movies
+            // Console.WriteLine($"{reply.Json}");
+            #endregion
+
+            #region API
+            string apiResponse = "";
+            long[] apiResults = new long[10];
+            for (int i = 0; i < 10; i++)
+            {
+                var stopwatchAPI = new System.Diagnostics.Stopwatch();
+                stopwatchAPI.Start();
+
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync("https://localhost:44311/api/top20"))
+                    {
+                        apiResponse = await response.Content.ReadAsStringAsync();
+                    }
+                }
+
+                stopwatchAPI.Stop();
+                apiResults[i] = stopwatchAPI.ElapsedMilliseconds;
+                Console.WriteLine($"API fetch time of top 20 movies: {stopwatchAPI.ElapsedMilliseconds} ms");
+            }
+            Console.WriteLine($"Average API fetch time: {apiResults.Average()}");
+
+            // Uncomment to see the complete string with the top 20 movies
+            // Console.WriteLine(apiResponse);
+            #endregion
+
 
             Console.ReadLine();
         }
