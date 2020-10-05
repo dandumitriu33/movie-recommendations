d3b434b2dd2238f9d3a31ddd42d7a073da7c7c90
Dan Dumitriu <dandumitriu33@gmail.com>
2020-10-05 13:26:21 +0300

    Add NextMovie model for DB

---
+++ MovieRecommendations/MoviesDataAccessLibrary/Models/NextMovie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesDataAccessLibrary.Models
+{
+    public class NextMovie
+    {
+        public int Id { get; set; }
+        public int CurrentMovieId { get; set; }
+        public int NextMovieId { get; set; }
+        public int Score { get; set; }
+    }
+}
