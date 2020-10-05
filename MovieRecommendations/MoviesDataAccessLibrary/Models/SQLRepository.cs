51d5ed36b0fabdd1b316d24fd2d7627d6d011ed5
Dan Dumitriu <dandumitriu33@gmail.com>
2020-10-05 15:13:03 +0300

    Fix issue - personalized picks based on content rating descending

--- MovieRecommendations/MoviesDataAccessLibrary/Models/SQLRepository.cs
+++ MovieRecommendations/MoviesDataAccessLibrary/Models/SQLRepository.cs
@@ -61,7 +61,7 @@ namespace MoviesDataAccessLibrary.Models
 
         public IEnumerable<Movie> GetDistanceRecommendation(string mainGenre, double rating, int limit, int offset)
         {
-            var recommendedMovies = _context.Movies.Where(m => m.MainGenre == mainGenre && m.Rating > rating - 2).OrderByDescending(m => m.ReleaseYear).ThenBy(m => m.Rating).Skip(offset).Take(limit);
+            var recommendedMovies = _context.Movies.Where(m => m.MainGenre == mainGenre && m.Rating > rating - 2).OrderByDescending(m => m.ReleaseYear).ThenByDescending(m => m.Rating).Skip(offset).Take(limit);
 
             return recommendedMovies;
         }
