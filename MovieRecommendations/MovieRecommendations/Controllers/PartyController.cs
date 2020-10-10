28826ff5a05eed4794656e0008d3088229ac258e
Dan Dumitriu <dandumitriu33@gmail.com>
2020-10-10 14:54:04 +0300

    Add watching parties Controller and View

---
+++ MovieRecommendations/MovieRecommendations/Controllers/PartyController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieRecommendations.Controllers
+{
+    public class PartyController : Controller
+    {
+        public IActionResult AllParties(string userEmail)
+        {
+            return View();
+        }
+    }
+}
