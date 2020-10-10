e4bcbb4fe3ac55f15ae085cfe028cd301940112a
Dan Dumitriu <dandumitriu33@gmail.com>
2020-10-10 15:19:00 +0300

    Add Create Party Modal

--- MovieRecommendations/MovieRecommendations/Controllers/PartyController.cs
+++ MovieRecommendations/MovieRecommendations/Controllers/PartyController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MoviesDataAccessLibrary.Models;
 
 namespace MovieRecommendations.Controllers
 {
     public class PartyController : Controller
     {
+        private readonly IRepository _repository;
+
+        public PartyController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [Route("party/{userEmail}")]
         public IActionResult AllParties(string userEmail)
         {
-            return View();
+            List<Party> userParties = _repository.GetUserParties(userEmail);
+            return View(userParties);
         }
     }
 }
