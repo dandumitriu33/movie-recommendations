7ae874ceef8e40e97d48529d531dd6ffcf4db453
Dan Dumitriu <dandumitriu33@gmail.com>
2020-10-12 11:39:06 +0300

    Add Full Swiper Card draggable html & swiper.js

--- MovieRecommendations/MovieRecommendations/wwwroot/js/swiper.js
+++ MovieRecommendations/MovieRecommendations/wwwroot/js/swiper.js
@@ -99,8 +99,8 @@ async function fetchMovieBatch() {
 function loadSwiper(movie) {
     //console.log("LOAD SWIPER: batch index before load " + batchIndex);
     let element = `
-                    <div class="card mb-4" style="min-width: 12rem; max-width: 12rem">
-                        <img class="card-img-top" src="https://localhost:44318/img/${movie.mainGenre.toLowerCase()}.jpg" alt="Card image cap">
+                    <div class="card mb-4 swipeDraggable" draggable="true" style="min-width: 12rem; max-width: 12rem">
+                        <img class="card-img-top" src="https://localhost:44318/img/${movie.mainGenre.toLowerCase()}.jpg" alt="Card image cap" draggable="false">
                         <div class="card-body">
                             <h5 class="card-title">${movie.title} (${movie.releaseYear})</h5>
                             <p class="card-text">IMDB: ${movie.rating}</p>
