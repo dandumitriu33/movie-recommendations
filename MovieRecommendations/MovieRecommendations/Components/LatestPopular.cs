2046ec33035bc4276f372c42d040440d910a7a5d
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-04 13:58:30 +0200

    Update LatestPopular to use MovieViewModel

--- MovieRecommendations/MovieRecommendations/Components/LatestPopular.cs
+++ MovieRecommendations/MovieRecommendations/Components/LatestPopular.cs
@@ -20,12 +20,32 @@ namespace MovieRecommendations.Components
 
         public IViewComponentResult Invoke()
         {
-            IEnumerable<Movie> allMoviesFromDb = _repository.GetAllMoviesTop20();
-            AllMoviesViewModel allMovies = new AllMoviesViewModel
+            IEnumerable<Movie> top20FromDb = _repository.GetAllMoviesTop20();
+            //AllMoviesViewModel allMovies = new AllMoviesViewModel
+            //{
+            //    Movies = top20FromDb.OrderByDescending(m => m.ReleaseYear).ThenByDescending(m => m.Rating).ToList()
+            //};
+
+            // sort by release yar and then by rating descending
+            List<Movie> top20FromDbSorted = top20FromDb.OrderByDescending(m => m.ReleaseYear).ThenByDescending(m => m.Rating).ToList();
+
+            List<MovieViewModel> top20MovieViewModel = new List<MovieViewModel>();
+            foreach (var movie in top20FromDbSorted)
             {
-                Movies = allMoviesFromDb.OrderByDescending(m => m.ReleaseYear).ThenByDescending(m => m.Rating).ToList()
-            };
-            return View(allMovies);
+                MovieViewModel tempMovieViewModel = new MovieViewModel
+                {
+                    Id = movie.Id,
+                    Title = movie.Title,
+                    LengthInMinutes = movie.LengthInMinutes,
+                    ReleaseYear = movie.ReleaseYear,
+                    Rating = movie.Rating,
+                    MainGenre = movie.MainGenre,
+                    SubGenre1 = movie.SubGenre1,
+                    SubGenre2 = movie.SubGenre2
+                };
+                top20MovieViewModel.Add(tempMovieViewModel);
+            }
+            return View(top20MovieViewModel);
         }
     }
 }
