e0a230cee34290af422afec3dcb3d084a66b2918
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-04 10:49:57 +0200

    Remove Display Attributes from DAL Movie Entity

--- MovieRecommendations/MoviesDataAccessLibrary/Entities/Movie.cs
+++ MovieRecommendations/MoviesDataAccessLibrary/Entities/Movie.cs
@@ -10,27 +10,21 @@ namespace MoviesDataAccessLibrary.Entities
     {
         public int Id { get; set; }
         [Required]
-        [Display(Name = "Title")]
         [Column(TypeName =("varchar(150)"))]
         public string Title { get; set; }
         [Required]
-        [Display(Name = "Length in minutes")]
         public int LengthInMinutes { get; set; }
         [Required]
-        [Display(Name = "Release year")]
         public int ReleaseYear { get; set; }
         public double Rating { get; set; }
         [Required]
-        [Display(Name = "Main Genre")]
-        [MaxLength(20)]
+        [Column(TypeName = ("varchar(20)"))]
         public string MainGenre { get; set; }
         [Required]
-        [Display(Name = "Sub Genre 1")]
-        [MaxLength(20)]
+        [Column(TypeName = ("varchar(20)"))]
         public string SubGenre1 { get; set; }
         [Required]
-        [Display(Name = "Sub Genre 2")]
-        [MaxLength(20)]
+        [Column(TypeName = ("varchar(20)"))]
         public string SubGenre2 { get; set; }
     }
 }
---
+++ MovieRecommendations/MoviesDataAccessLibrary/Migrations/20201104084217_RemoveDisplayAttributesFromMovieEntityDAL.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesDataAccessLibrary.Migrations
+{
+    public partial class RemoveDisplayAttributesFromMovieEntityDAL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SubGenre2",
+                table: "Movies",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SubGenre1",
+                table: "Movies",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MainGenre",
+                table: "Movies",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SubGenre2",
+                table: "Movies",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SubGenre1",
+                table: "Movies",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MainGenre",
+                table: "Movies",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+        }
+    }
+}
