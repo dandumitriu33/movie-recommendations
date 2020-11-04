9c7ac5e25857d4ff9012dc08da6bef8b31198632
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-04 13:45:24 +0200

    Update Rabbit Hole to use MovieVM

--- MovieRecommendations/MovieRecommendations/Components/CurrentRabbitHole.cs
+++ MovieRecommendations/MovieRecommendations/Components/CurrentRabbitHole.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using MoviesDataAccessLibrary.Repositories;
+using MovieRecommendations.ViewModels;
 
 namespace MovieRecommendations.Components
 {
@@ -24,7 +25,8 @@ namespace MovieRecommendations.Components
 
         public IViewComponentResult Invoke()
         {
-            List<Movie> currentRabbitHole = new List<Movie>();
+            List<MovieViewModel> currentRabbitHole = new List<MovieViewModel>();
+
             // get the currently connected user email
             string userEmail = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.Name).Value.ToString();
 
@@ -32,18 +34,41 @@ namespace MovieRecommendations.Components
             History latestWatchedHistoryItem = _repository.GetLatestFromHistory(userEmail);
             Movie latestWatchedMovie = _repository.GetMovieByMovieId(latestWatchedHistoryItem.MovieId);
 
-            // add the Rabbit Hole entry point - the last movie in the History of the user
-            currentRabbitHole.Add(latestWatchedMovie);
+            // map and add the Rabbit Hole entry point - the last movie in the History of the user
+            MovieViewModel latestWatchedMovieViewModel = new MovieViewModel
+            {
+                Id = latestWatchedMovie.Id,
+                Title = latestWatchedMovie.Title,
+                LengthInMinutes = latestWatchedMovie.LengthInMinutes,
+                ReleaseYear = latestWatchedMovie.ReleaseYear,
+                Rating = latestWatchedMovie.Rating,
+                MainGenre = latestWatchedMovie.MainGenre,
+                SubGenre1 = latestWatchedMovie.SubGenre1,
+                SubGenre2 = latestWatchedMovie.SubGenre2
+            };
+            currentRabbitHole.Add(latestWatchedMovieViewModel);
 
             // while there are next movies, always get the strongest nextMovie for each and add it to the Rabbit Hole
             List<NextMovie> nextMovies = new List<NextMovie>();
+
             // based on the latest movie, get the list of nextMovies
             nextMovies = _repository.GetNextMoviesForMovieById(latestWatchedMovie.Id);
             int counter = 0;
             while (nextMovies.Count() > 0 && counter < 9)
             {
                 Movie tempMovie = _repository.GetMovieByMovieId(nextMovies[0].NextMovieId);
-                currentRabbitHole.Add(tempMovie);
+                MovieViewModel tempMovieViewModel = new MovieViewModel
+                {
+                    Id = tempMovie.Id,
+                    Title = tempMovie.Title,
+                    LengthInMinutes = tempMovie.LengthInMinutes,
+                    ReleaseYear = tempMovie.ReleaseYear,
+                    Rating = tempMovie.Rating,
+                    MainGenre = tempMovie.MainGenre,
+                    SubGenre1 = tempMovie.SubGenre1,
+                    SubGenre2 = tempMovie.SubGenre2
+                };
+                currentRabbitHole.Add(tempMovieViewModel);
                 nextMovies.Clear();
                 nextMovies = _repository.GetNextMoviesForMovieById(tempMovie.Id);
                 counter++;
