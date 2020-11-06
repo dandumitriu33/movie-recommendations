c0fa260a4641e6812c122ae3db096b2c1f336385
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-06 19:34:08 +0200

    Update AllLatestHighestRated to use Automapper

--- MovieRecommendations/MovieRecommendations/Components/AllLatestHighestRated.cs
+++ MovieRecommendations/MovieRecommendations/Components/AllLatestHighestRated.cs
@@ -1,44 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using MovieRecommendations.ViewModels;
 using MoviesDataAccessLibrary.Entities;
-using System;
+using MoviesDataAccessLibrary.Repositories;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using MoviesDataAccessLibrary.Repositories;
 
 namespace MovieRecommendations.Components
 {
     public class AllLatestHighestRated : ViewComponent
     {
         private readonly IRepository _repository;
+        private readonly IMapper _mapper;
 
-        public AllLatestHighestRated(IRepository repository)
+        public AllLatestHighestRated(IRepository repository,
+                                     IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         public IViewComponentResult Invoke()
         {
             IEnumerable<Movie> allMoviesFromDb = _repository.GetAllMovies();
-            // sort by release yar and then by rating descending
+            // sort by release year and then by rating descending
             List<Movie> allMoviesFromDbSorted = allMoviesFromDb.OrderByDescending(m => m.ReleaseYear).ThenByDescending(m => m.Rating).ToList();
-            List<MovieViewModel> allMovies = new List<MovieViewModel>();
-            foreach (var movie in allMoviesFromDbSorted)
-            {
-                MovieViewModel tempMovieViewModel = new MovieViewModel
-                {
-                    Id = movie.Id,
-                    Title = movie.Title,
-                    LengthInMinutes = movie.LengthInMinutes,
-                    ReleaseYear = movie.ReleaseYear,
-                    Rating = movie.Rating,
-                    MainGenre = movie.MainGenre,
-                    SubGenre1 = movie.SubGenre1,
-                    SubGenre2 = movie.SubGenre2
-                };
-                allMovies.Add(tempMovieViewModel);
-            }
+            List<MovieViewModel> allMovies = _mapper.Map<List<Movie>, List<MovieViewModel>>(allMoviesFromDbSorted);
             return View(allMovies);
         }
     }
