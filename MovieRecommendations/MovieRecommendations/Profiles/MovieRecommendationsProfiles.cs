3609548615d84d3a5b707ece357e6064bd599ad9
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-06 19:11:35 +0200

    Add Automapper and test on Latest Top 20

---
+++ MovieRecommendations/MovieRecommendations/Profiles/MovieRecommendationsProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MovieRecommendations.ViewModels;
+using MoviesDataAccessLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRecommendations.Profiles
+{
+    public class MovieRecommendationsProfiles : Profile
+    {
+        public MovieRecommendationsProfiles()
+        {
+            CreateMap<Movie, MovieViewModel>()
+                .ForMember(vm => vm.Title, o => o.MapFrom(s => s.Title))
+                .ReverseMap();
+        }
+    }
+}
