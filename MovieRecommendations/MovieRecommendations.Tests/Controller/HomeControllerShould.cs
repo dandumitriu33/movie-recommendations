3d2d7b6c50a858a7e28de81664c934051a4df273
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-12 14:41:10 +0200

    Add HomeController unit test Privacy ViewResult return

---
+++ MovieRecommendations/MovieRecommendations.Tests/Controller/HomeControllerShould.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using Microsoft.Extensions.Logging;
+using MovieRecommendations.Controllers;
+using MoviesDataAccessLibrary.Repositories;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieRecommendations.Tests.Controller
+{
+    public class HomeControllerShould
+    {
+        [Fact]
+        public void ReturnViewForPrivacy()
+        {
+            var mockLogger = new Mock<ILogger<HomeController>>();
+            var mockRepository = new Mock<IRepository>();
+            var mockMapper = new Mock<IMapper>();
+
+            // sut = System Under Test
+            var sut = new HomeController(mockLogger.Object, mockRepository.Object, mockMapper.Object);
+
+            IActionResult result = sut.Privacy();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
