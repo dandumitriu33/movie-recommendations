df0a233c9be1afa1da218e8746e08636709fd48e
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-12 15:37:36 +0200

    Refactor UserHistory for no history users + test

--- MovieRecommendations/MovieRecommendations.Tests/Controller/UserControllerShould.cs
+++ MovieRecommendations/MovieRecommendations.Tests/Controller/UserControllerShould.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using Moq;
+using MoviesDataAccessLibrary.Repositories;
+using AutoMapper;
+using MovieRecommendations.Interfaces;
+using MovieRecommendations.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MovieRecommendations.Tests.Controller
 {
@@ -11,7 +16,16 @@ namespace MovieRecommendations.Tests.Controller
         [Fact]
         public void ReturnViewForUserHistory()
         {
-            
+            var mockRepository = new Mock<IRepository>();
+            var mockMapper = new Mock<IMapper>();
+            var mockBuilder = new Mock<IPersonalizedRecommendationsBuilder>();
+
+            // sut = System Under Test
+            var sut = new UserController(mockRepository.Object, mockMapper.Object, mockBuilder.Object);
+
+            IActionResult result = sut.UserHistory("john@email.com");
+
+            Assert.IsType<ViewResult>(result);
         }
 
     }
