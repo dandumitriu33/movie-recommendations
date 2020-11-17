b5656bdee1baf67a24ee7c7e4aba2218eede2d44
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-17 19:54:39 +0200

    Add Test - UI, AddMovie - LengthInMinutes validation correct

--- MovieRecommendations/MovieRecommendations.UITests/AddMovieTests.cs
+++ MovieRecommendations/MovieRecommendations.UITests/AddMovieTests.cs
@@ -25,7 +25,7 @@ namespace MovieRecommendations.UITests
         }
 
         [Fact]
-        public void ShouldValidateAddMovieDetails()
+        public void ShouldValidateAddMovieDetailsTitleMissing()
         {
             _driver.Navigate().GoToUrl("http://localhost:51264/home/addmovie");
 
@@ -74,6 +74,60 @@ namespace MovieRecommendations.UITests
 
             Assert.Equal("The movie title is required.", firstErrorMessage.Text);
         }
+
+        [Fact]
+        public void ShouldValidateAddMovieDetailsLengthInMinutesMissing()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:51264/home/addmovie");
+
+            IWebElement title = _driver.FindElement(By.Name("Title"));
+            title.SendKeys("Fun Test Movie");
+
+            DelayForDemo();
+
+            // LengthInMinutes left out
+
+            _driver.FindElement(By.Name("ReleaseYear")).SendKeys("2016");
+
+            DelayForDemo();
+
+            IWebElement rating = _driver.FindElement(By.Name("Rating"));
+            rating.SendKeys("6.5");
+
+            DelayForDemo();
+
+            // including select elements https://stackoverflow.com/questions/5278281/how-to-select-an-option-from-drop-down-using-selenium-webdriver-c
+            IWebElement mainGenre = _driver.FindElement(By.Name("MainGenre"));
+            var mainGenreElement = new SelectElement(mainGenre);
+            mainGenreElement.SelectByText("Comedy");
+
+            DelayForDemo();
+
+            IWebElement subGenre1 = _driver.FindElement(By.Name("SubGenre1"));
+            var subGenre1Element = new SelectElement(subGenre1);
+            subGenre1Element.SelectByText("Adventure");
+
+            DelayForDemo();
+
+            IWebElement subGenre2 = _driver.FindElement(By.Name("SubGenre2"));
+            var subGenre2Element = new SelectElement(subGenre2);
+            subGenre2Element.SelectByText("Mystery");
+
+            DelayForDemo();
+
+            _driver.FindElement(By.Name("SubmitNewMovie")).Click();
+
+            DelayForDemo();
+
+            Assert.Equal("Add movie - MovieRecommendations", _driver.Title);
+
+            IWebElement firstErrorMessage = _driver.FindElement(By.CssSelector(".validation-summary-errors ul > li"));
+
+            Assert.Equal("The movie runtime (minutes) is required.", firstErrorMessage.Text);
+        }
+
+        // The movie release year is required.
+        // The movie rating is required.
 
         /// <summary>
         /// Introduces a 1 second delay for demo purposes. The action happens to fast to observe otherwise.
