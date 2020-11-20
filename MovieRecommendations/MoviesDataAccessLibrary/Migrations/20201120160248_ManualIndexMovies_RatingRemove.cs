37bac011292ed53da781bb2b2164ca9300dc982c
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-20 18:07:04 +0200

    Add DB Manual Index on Histories - Email

---
+++ MovieRecommendations/MoviesDataAccessLibrary/Migrations/20201120160248_ManualIndexMovies_RatingRemove.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesDataAccessLibrary.Migrations
+{
+    public partial class ManualIndexMovies_RatingRemove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "Movies_Rating",
+                table: "Movies");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+            name: "Movies_Rating",
+            table: "Movies",
+            column: "Rating");
+        }
+    }
+}
