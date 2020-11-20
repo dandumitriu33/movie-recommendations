f00a3484927ef4cfba5838bf9be1f3ac32f48c33
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-20 18:26:40 +0200

    Add Manual DB Index - Parties - Id

---
+++ MovieRecommendations/MoviesDataAccessLibrary/Migrations/20201120162401_ManualIndexParties_Id.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesDataAccessLibrary.Migrations
+{
+    public partial class ManualIndexParties_Id : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "Parties_Id",
+                table: "Parties",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "Parties_Id",
+                table: "Parties");
+        }
+    }
+}
