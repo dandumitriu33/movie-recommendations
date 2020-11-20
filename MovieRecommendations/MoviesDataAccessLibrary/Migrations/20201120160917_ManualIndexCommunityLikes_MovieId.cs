c2489729885a983b1094e396f13e59549b076bc5
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-20 18:11:54 +0200

    Add DB Index - CommunityLikes on MovieId

---
+++ MovieRecommendations/MoviesDataAccessLibrary/Migrations/20201120160917_ManualIndexCommunityLikes_MovieId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesDataAccessLibrary.Migrations
+{
+    public partial class ManualIndexCommunityLikes_MovieId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "CommunityLikes_MovieId",
+                table: "CommunityLikes",
+                column: "MovieId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "CommunityLikes_MovieId",
+                table: "CommunityLikes");
+        }
+    }
+}
