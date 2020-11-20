b61ab32dcb1b6290af0b928eb9456048ae39751c
Dan Dumitriu <dandumitriu33@gmail.com>
2020-11-20 18:22:38 +0200

    Add Manual Index - NextMovies - CurrentMovieId

---
+++ MovieRecommendations/MoviesDataAccessLibrary/Migrations/20201120162051_ManualIndexNextMovies_CurrentMovieId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesDataAccessLibrary.Migrations
+{
+    public partial class ManualIndexNextMovies_CurrentMovieId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "NextMovies_CurrentMovieId",
+                table: "NextMovies",
+                column: "CurrentMovieId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "NextMovies_CurrentMovieId",
+                table: "NextMovies");
+        }
+    }
+}
